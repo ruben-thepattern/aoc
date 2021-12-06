9e1eda2d06b817b87e83d50ca8969304340f813b
Ruben Holen <ruben@thepattern.com>
2021-12-06 11:59:50 +0000

    golfed day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -7,11 +7,8 @@ fun main() {
 private fun part1(school: List<Int>) = school.evolve(80)
 private fun part2(school: List<Int>) = school.evolve(256)
 
-private fun List<Int>.evolve(days: Int): Long {
-    val school = (0..8).map { n -> count { it == n }.toLong() }.toLongArray()
-    repeat(days) {
-        val d = it.mod(school.size)
-        school[(d + 7).mod(school.size)] += school[d]
-    }
-    return school.sum()
-}
+private fun List<Int>.evolve(days: Int, rate: Int = 8) =
+    (0 until days).fold((0..rate).map { n -> count { it == n }.toLong() }.toLongArray()) { school, it ->
+        school[(it % school.size + (rate - 1)) % school.size] += school[it % school.size]
+        school
+    }.sum()
